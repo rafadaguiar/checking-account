32354274cccbccff6082ba40a44823b9e8da4743
rafael.aguiar <rafael@alumni.ubc.ca>
2017-04-22 13:17:28 -0300

    chore(mongoRemote): update references of remote mongo to mongo Atlas

--- src/main/scala/com/rfna/checking_account/db/MongoDBBaseDAO.scala
+++ src/main/scala/com/rfna/checking_account/db/MongoDBBaseDAO.scala
@@ -3,11 +3,11 @@ package com.rfna.checking_account.db
 import com.rfna.checking_account.utils.{Configurations, Environment}
 import io.circe.optics.JsonPath._
 import org.mongodb.scala.MongoClient
-import org.slf4j.LoggerFactory
 
 import scala.util.Properties.envOrNone
 
 trait MongoDBBaseDAO {
+  System.setProperty("org.mongodb.async.type", "netty")
   protected val conf = Configurations.getConf("/mongo_conf.yaml")
   protected val MONGO_URI = getMongoUri()
   protected val DATABASE_NAME = root.database_name.string.getOption(conf).get
@@ -18,9 +18,8 @@ trait MongoDBBaseDAO {
   private def getMongoUri(): String = {
     val mongoUri = root.mongo_uri.string.getOption(conf).get
     if (Environment.getEnv() == "production" || Environment.getEnv() == "ci") {
-      val user = envOrNone("dbuser").get
-      val password = envOrNone("dbpassword").get
-      mongoUri.replaceFirst("<dbuser>", user).replaceFirst("<dbpassword>", password)
+      val password = envOrNone("PASSWORD").get
+      mongoUri.replaceFirst("<PASSWORD>", password)
     } else {
       mongoUri
     }
