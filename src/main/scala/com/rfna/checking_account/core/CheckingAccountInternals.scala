c64f2f257cc4d849685b1d62255366454fee8306
rafael.aguiar <rafael@alumni.ubc.ca>
2019-02-02 10:54:44 -0500

    style(CheckingAccountInternals): shorter lines

--- src/main/scala/com/rfna/checking_account/core/CheckingAccountInternals.scala
+++ src/main/scala/com/rfna/checking_account/core/CheckingAccountInternals.scala
@@ -11,7 +11,8 @@ trait CheckingAccountInternals {
     Balance(operations.foldLeft(BigDecimal.decimal(0.0))(_ + _.value))
   }
 
-  def getStatementsBetween(operations: List[Operation], start: LocalDate, end: LocalDate): List[Statement] = {
+  def getStatementsBetween(operations: List[Operation], start: LocalDate, end: LocalDate)
+  : List[Statement] = {
     getStatements(operations)
       .filter { statement =>
         (statement.date.isEqual(start) || statement.date.isAfter(start)) &&
@@ -38,36 +39,37 @@ trait CheckingAccountInternals {
         loop(tail, start, Some(statement), debts)
       case statement :: tail if debtCeased(statement.balance, start) =>
         loop(tail, None, None, Debt.fromPeriod(start, end) :: debts)
-      case SimpleStatement(date, balance) :: tail =>
+      case SimpleStatement(_, _) :: tail =>
         loop(tail, start, end, debts)
     }
     loop(simpleStatements, start = None, end = None, debts = Nil).reverse
   }
 
   private def getStatements(operations: List[Operation]): List[Statement] = {
-    val operationsGroupedAndSortedByDate = getOperationsGroupedAndSortedByDate(operations)
-
     @tailrec
     def loop(
-      operationsGroupedAndSortedByDay: List[(LocalDate, List[Operation])],
+      operationsGroupedAndSortedByDate: List[(LocalDate, List[Operation])],
       accBalance: BigDecimal,
       statements: List[Statement]
-    ): List[Statement] = operationsGroupedAndSortedByDay match {
+    ): List[Statement] = operationsGroupedAndSortedByDate match {
       case Nil => statements
       case (date, operations) :: tail =>
         val newBalance = accBalance + getBalance(operations).value
         loop(tail, newBalance, Statement(date, operations, newBalance) :: statements)
-
     }
+
+    val operationsGroupedAndSortedByDate = getOperationsGroupedAndSortedByDate(operations)
     loop(operationsGroupedAndSortedByDate, accBalance = 0, statements = Nil).reverse
   }
 
-  private def getOperationsGroupedAndSortedByDate(operations: List[Operation]): List[(LocalDate, List[Operation])] = {
-    val operationsGroupedByDay = groupOperationsByDate(operations)
-    operationsGroupedByDay.sortWith { case (op1, op2) => op1._1.isBefore(op2._1) }
+  private def getOperationsGroupedAndSortedByDate(operations: List[Operation])
+  : List[(LocalDate, List[Operation])] = {
+    val operationsGroupedByDate = groupOperationsByDate(operations)
+    operationsGroupedByDate.sortWith { case (op1, op2) => op1._1.isBefore(op2._1) }
   }
 
-  private def groupOperationsByDate(operations: List[Operation]): List[(LocalDate, List[Operation])] = {
+  private def groupOperationsByDate(operations: List[Operation])
+  : List[(LocalDate, List[Operation])] = {
     operations.groupBy(_.date).toList
   }
 
@@ -79,9 +81,12 @@ trait CheckingAccountInternals {
     if (start.isDefined) Debt(start.get.date, None, end.get.balance.abs) :: debts else debts
   }
 
-  private def foundADebtStart(balance: BigDecimal, start: Option[SimpleStatement]): Boolean = balance < 0 && start.isEmpty
+  private def foundADebtStart(balance: BigDecimal, start: Option[SimpleStatement])
+  : Boolean = balance < 0 && start.isEmpty
 
-  private def debtContinued(balance: BigDecimal, start: Option[SimpleStatement]): Boolean = balance < 0 && start.isDefined
+  private def debtContinued(balance: BigDecimal, start: Option[SimpleStatement])
+  : Boolean = balance < 0 && start.isDefined
 
-  private def debtCeased(balance: BigDecimal, start: Option[SimpleStatement]): Boolean = balance >= 0 && start.isDefined
+  private def debtCeased(balance: BigDecimal, start: Option[SimpleStatement])
+  : Boolean = balance >= 0 && start.isDefined
 }
